4ba22c194b35684227a01c42d20891851d378925
totarochristian <totaro.christian.18@gmail.com>
2023-03-31 19:34:51 +0200

    Define the header and the content style of the formRight div in case of contact selected

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-            selectedContactIndex: -1,
+            contactOpened: { id: -1 },
             profile:{
                 name: 'Christian',
                 avatar: "./assets/images/profiles/avatar_2.jpg"
@@ -17,5 +17,7 @@ createApp({
         const tmp = await LoadJsonFile();
         //Deep-copy of tmp.contacts array in this.contacts var
         this.contacts = JSON.parse(JSON.stringify(tmp.contacts));
+        //Set the opened contact as the first element of the contacts array
+        this.contactOpened = this.contacts[0];
     }
 }).mount("#app");
