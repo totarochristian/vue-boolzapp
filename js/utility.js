452c836fcc9a159d72d77981484e5857b107f38a
totarochristian <totaro.christian.18@gmail.com>
2023-04-03 17:52:54 +0200

    Add the function GetRandomInt inside the utility file

--- js/utility.js
+++ js/utility.js
@@ -90,3 +90,15 @@ function PlayAudio(audioSrc){
     const aud = new Audio(audioSrc);
     aud.play();
 }
+
+/**
+ * Function that will generate a random int
+ * @param {bigint} max Max value of the interval
+ * @param {bigint} min Min value of the interval
+ * @returns {bigint} Generated random int value
+ */
+function GetRandomInt(max,min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
