9cce1b479167cbd45b6c384560d18e555bd846b4
totarochristian <totaro.christian.18@gmail.com>
2023-04-03 12:39:17 +0200

    Add comments to explain the emthods inside the vue app

--- js/script.js
+++ js/script.js
@@ -12,10 +12,17 @@ createApp({
         }
     },
     methods:{
+        /**
+         * Function used to open a contact in the form right div.
+         * @param {*} index Index of the contact (in the array of contacts) to be opened.
+         */
         OpenContact(index){
-            //Set the opened contact as the index-element of the contacts array
+            //Set the opened contact as the index-element of the contacts array.
             this.contactOpened = this.contacts[index];
         },
+        /**
+         * Function used to send a new message to an opened contact.
+         */
         SendNewMessage(){
             if(this.contactOpened.newMessageToSend){
                 const tmpDate = GetCurrentDateTimeStringFormatted();
@@ -26,7 +33,10 @@ createApp({
                     message: this.contactOpened.newMessageToSend,
                     status: "sent"
                 }
+                //Add the message to the array of messages
                 this.contactOpened.messages.push(obj);
+                //Cancel the value inside the newMessageToSend
+                this.contactOpened.newMessageToSend = '';
             }
         }
     },
